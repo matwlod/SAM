1c1f7f3150c9d3efc69cda1d89ead91783bd4841
unknown <student@L405-3.helium.com>
2024-05-20 14:40:23 +0200

    lab2

---
+++ lab2/source/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    let html = "<body><h1>Hello World Player</h1>"
+
+    if(req.query.videoFile) {
+        html += `<video id="videoPlayer" controls><source src=${req.query.videoFile}></video><BR><BR>`
+        html += `<button id="videoCancel" onClick="cancelVideo()">cancel video</button><BR><BR>`
+    }
+
+    if(req.query.audioFile) {
+        html += `<audio id="audioPlayer" controls><source src=${req.query.audioFile}></audio><BR><BR>`
+        html += `<button id="audioCancel" onClick="cancelAudio()">cancel audio</button><BR><BR>`
+    }
+
+    if(req.query.imgFile) {
+        html += `<img id="posterImage" src=${req.query.imgFile}><BR><BR>`
+        html += `<button id="imgCancel" onClick="cancelImg()">cancel image</button><BR><BR>`
+    
+    }
+
+
+    html += `<script>
+        function cancelVideo() {
+            document.getElementById("videoPlayer").src="cancel.mp4";
+        }
+        function cancelImg() {
+            document.getElementById("posterImage").src="cancel.img";
+        }
+
+        function cancelAudio() {
+            document.getElementById("audioPlayer").src="cancel.mp3";
+        }
+
+    </script>
+`
+    html +="</body>"
+    res.send(html)
+})
+
+app.listen(4080)
